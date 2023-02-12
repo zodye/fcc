503f7cf2a8298affcdf7199c310d208825406234
Jodie Anderson <38399103+zodye@users.noreply.github.com>
2023-02-12 18:52:03 +0700

    Update salon.sh

--- salon/salon.sh
+++ salon/salon.sh
@@ -17,7 +17,9 @@ MENU() {
   done
 
   read SERVICE_ID_SELECTED
-  if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]] || [[ $SERVICE_ID_SELECTED -lt 1 || $SERVICE_ID_SELECTED -gt 6 ]]
+
+  SERVICES_COUNT=$($PSQL "SELECT COUNT(*) FROM services")
+  if [[ ! $SERVICE_ID_SELECTED =~ ^[0-9]+$ ]] || [[ $SERVICE_ID_SELECTED -lt 1 || $SERVICE_ID_SELECTED -gt $SERVICES_COUNT ]]
   then
     MENU "Please enter valid service number."
   
@@ -34,7 +36,7 @@ MENU() {
       REGISTER_NEW_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
     
     else
-      echo "Your name: $CUSTOMER_NAME"
+      echo "Your name: $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')"
     fi
 
     echo -e "\nWhat time is the appointment?"
@@ -45,7 +47,7 @@ MENU() {
     APPOINTMENT_SUCCESS_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
     if [[ $APPOINTMENT_SUCCESS_RESULT == "INSERT 0 1" ]]
     then
-      echo -e "\nI have put you down for a $(echo $SERVICE_NAME | sed -r 's/^ *| *$//g') at $SERVICE_TIME, $CUSTOMER_NAME."
+      echo -e "\nI have put you down for a $(echo $SERVICE_NAME | sed -r 's/^ *| *$//g') at $SERVICE_TIME, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')."
     fi
   fi
 }
